adbec7437bf98d44241bf69ac148170194402755
caiyang123 <32664687+caiyang123@users.noreply.github.com>
2019-06-05 16:15:51 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <ul>
+      <items :model="item" v-for="(item, index) in list" :key="index"></items>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Items from "./components/Items.vue";
+
+export default {
+  name: "app",
+  data() {
+    return {
+      list: [
+        {
+          menuName: "项目管理",
+          childTree: [
+            {
+              menuName: "项目进度",
+              childTree: [
+                {
+                  menuName: "项目一",
+                  childTree: [{ menuName: "详细信息" }]
+                }
+              ]
+            },
+            {
+              menuName: "任务安排"
+            }
+          ]
+        },
+        {
+          menuName: "数据统计"
+        },
+        {
+          menuName: "人员管理"
+        }
+      ]
+    };
+  },
+  components: {
+    Items
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Items.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <span @click="toggle">
+            {{ model.menuName }}
+        </span>
+        <ul v-if="isFolder" v-show="isShow">
+            <items v-for="(item, index) in model.childTree" :model="item" :key="index"></items>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'items',
+        props: ['model'],
+        data() {
+            return {
+                isShow: false
+            }
+        },
+        computed: {
+            isFolder() {
+                return this.model.childTree && this.model.childTree.length;
+            }
+        },
+        methods: {
+            toggle() {
+                this.isShow = !this.isShow;
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
